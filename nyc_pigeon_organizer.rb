ccc1869890a3de5a4f036ed3476808441c512bb5
Brian Sahota <bpsahota@gmail.com>
2020-09-09 14:29:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,8 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value), final_array|
     # binding.pry
     value.each do |inner_key, names|
+    names.each do |name|
       binding.pry 
-      names.each do |name|
+    end
   end
   final_array
 end 
