7d5c7891d77ac951926e5fae23354ddc969ed957
Brian Sahota <bpsahota@gmail.com>
2020-09-09 14:19:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, values), final_array|
-    afinal_array = v + 3
+    final_array = v + 3
   end
   binding.pry
 end
